2624198113bd3bd99dd9f3b8a84a322a069c1b6f
TheBlueLines <davidfeher42@gmail.com>
2024-02-13 18:02:31 +0100

    Add project files.

---
+++ MapManager.cs
@@ -0,0 +1,60 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace W2C
+{
+	public class MapManager
+	{
+		private static int selectedMaterial = 0;
+		private static Dictionary<Vector2, int> placedBlocks = new();
+		public static void DrawCursor(Vector2 location)
+		{
+			float thick = 2;
+			Raylib.DrawLineEx(location * 32 + new Vector2(-16, -16), location * 32 + new Vector2(-16, -8), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(-16, -16), location * 32 + new Vector2(-8, -16), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(-16, 16), location * 32 + new Vector2(-16, 8), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(-16, 16), location * 32 + new Vector2(-8, 16), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(16, -16), location * 32 + new Vector2(8, -16), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(16, -16), location * 32 + new Vector2(16, -8), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(16, 16), location * 32 + new Vector2(8, 16), thick, Color.White);
+			Raylib.DrawLineEx(location * 32 + new Vector2(16, 16), location * 32 + new Vector2(16, 8), thick, Color.White);
+		}
+		public static void DrawBlock(Vector2 location, int material, int alpha = 255)
+		{
+			if (material == 0)
+			{
+				Raylib.DrawLineEx(location * 32 + new Vector2(16, 16), location * 32 + new Vector2(-16, -16), 2, new Color(230, 41, 55, alpha));
+				Raylib.DrawLineEx(location * 32 + new Vector2(16, -16), location * 32 + new Vector2(-16, 16), 2, new Color(230, 41, 55, alpha));
+				Raylib.DrawLineEx(location * 32 + new Vector2(16, 16), location * 32 + new Vector2(-16, 16), 2, new Color(190, 33, 55, alpha));
+				Raylib.DrawLineEx(location * 32 + new Vector2(16, 16), location * 32 + new Vector2(16, -16), 2, new Color(190, 33, 55, alpha));
+				Raylib.DrawLineEx(location * 32 + new Vector2(-16, -16), location * 32 + new Vector2(-16, 16), 2, new Color(190, 33, 55, alpha));
+				Raylib.DrawLineEx(location * 32 + new Vector2(-16, -16), location * 32 + new Vector2(16, -16), 2, new Color(190, 33, 55, alpha));
+			}
+		}
+		public static void DrawMap()
+		{
+			foreach (KeyValuePair<Vector2, int> keyValuePair in placedBlocks)
+			{
+				DrawBlock(keyValuePair.Key, keyValuePair.Value);
+			}
+		}
+		public static void DrawPreview(Vector2 position)
+		{
+			DrawBlock(position, selectedMaterial, 100);
+		}
+		public static void UserInput(Vector2 position)
+		{
+			if (Raylib.IsMouseButtonDown(MouseButton.Left))
+			{
+				placedBlocks[position] = selectedMaterial;
+			}
+			else if (Raylib.IsMouseButtonDown(MouseButton.Right))
+			{
+				if (placedBlocks.ContainsKey(position))
+				{
+					placedBlocks.Remove(position);
+				}
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace W2C
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Image icon = Raylib.LoadImage("hammer.png");
+			Raylib.SetWindowState(ConfigFlags.ResizableWindow);
+			Raylib.InitWindow(800, 480, "Worbench 2D Creator");
+			Raylib.SetWindowIcon(icon);
+			Camera2D camera = new Camera2D() { Target = Vector2.Zero, Offset = Vector2.Zero, Rotation = 0, Zoom = 2 };
+			while (!Raylib.WindowShouldClose())
+			{
+				Vector2 render = new(Raylib.GetRenderWidth(), Raylib.GetRenderHeight());
+				Vector2 mouseLocation = (Raylib.GetMousePosition() - render / 2) / camera.Zoom;
+				camera.Offset = new Vector2(render.X / 2, render.Y / 2);
+				Raylib.BeginDrawing();
+				Raylib.ClearBackground(Color.Black);
+				Raylib.BeginMode2D(camera);
+				MapManager.DrawMap();
+				Vector2 need = new Vector2((int)MathF.Round(mouseLocation.X / 32), (int)MathF.Round(mouseLocation.Y / 32));
+				MapManager.DrawPreview(need);
+				MapManager.DrawCursor(need);
+				MapManager.UserInput(need);
+				Raylib.EndMode2D();
+				Raylib.DrawFPS(10, 10);
+				Raylib.EndDrawing();
+			}
+			Raylib.CloseWindow();
+		}
+	}
+}
